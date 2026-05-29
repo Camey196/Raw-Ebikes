8c3e28e31509938c22e337c98350e74ad9c8e9e8
Camey196 <Camey196@users.noreply.github.com>
2026-05-29 13:24:43 +0000

    deploy: 765af72d2e7c1e88a483cd59e120dc9d7e3a0095

---
+++ server/assets/_tanstack-start-manifest_v-BeZaR3Pf.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/runner/work/Raw-Ebikes/Raw-Ebikes/src/routes/__root.tsx", children: ["/", "/checkout/success"], preloads: ["/assets/index-R8KkOSlh.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-R8KkOSlh.js" } }] }, "/": { filePath: "/home/runner/work/Raw-Ebikes/Raw-Ebikes/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BjGodSrA.js", "/assets/petals-B9AlIZvF.js"] }, "/checkout/success": { filePath: "/home/runner/work/Raw-Ebikes/Raw-Ebikes/src/routes/checkout.success.tsx", children: void 0, preloads: ["/assets/checkout.success-CPjxhJya.js", "/assets/petals-B9AlIZvF.js"] } } });
+export {
+  tsrStartManifest
+};
