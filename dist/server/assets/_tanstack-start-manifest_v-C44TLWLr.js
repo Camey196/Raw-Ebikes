a56b780591b3f15abcd136d0319cee635d39420d
Camey196 <cam.chattwood@gmail.com>
2026-05-29 17:53:33 +0100

    Convert to static SPA

--- dist/server/assets/_tanstack-start-manifest_v-C44TLWLr.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Desktop/New folder (2)/src/routes/__root.tsx", children: ["/", "/checkout/success"], preloads: ["/assets/index-BqGjoUDZ.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BqGjoUDZ.js" } }] }, "/": { filePath: "D:/Desktop/New folder (2)/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-m0c3wDP2.js", "/assets/petals-CBCUL_TZ.js"] }, "/checkout/success": { filePath: "D:/Desktop/New folder (2)/src/routes/checkout.success.tsx", children: void 0, preloads: ["/assets/checkout.success-CnwWJlug.js", "/assets/petals-CBCUL_TZ.js"] } } });
-export {
-  tsrStartManifest
-};
